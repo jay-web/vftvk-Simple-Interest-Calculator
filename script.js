d3a7090d39ee0287969e58ea959036d3ac57b9c8
jay-web <jay.developer12@gmail.com>
2021-08-12 11:56:07 +0530

    Edited css and added basic calculation features using javascript

--- script.js
+++ script.js
@@ -1,6 +1,36 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
+let principal = document.getElementById("principal");
+let rateOfInterest = document.getElementById("rate");
+let noOfYears;
+let form = document.querySelector("form");
+
+window.onload = () => {
+  
+  // setting value of range slider to display on page
+  document.getElementById("rangeValue").innerHTML = rateOfInterest.value + "%";  
+
+  // setting value of slider on change event
+  rateOfInterest.addEventListener("change", () => {
+    document.getElementById("rangeValue").innerHTML = parseFloat(rateOfInterest.value) + "%";
+  });
+
+  // binding submit event with form
+  form.addEventListener("submit", compute);
+  
+};
+
+function compute(e) {
+    e.preventDefault();
     
-}
-        
+    noOfYears = document.getElementById("years");
+    let years = parseInt(noOfYears.options[noOfYears.selectedIndex].text);
+    
+    let output = principal.value * ( parseFloat(rateOfInterest.value) / 100) * years;
+    let message = `If you deposit ${principal.value},
+                    at the interest rate of ${rateOfInterest.value}%. 
+                    You will receive an amount of ${output},
+                    in the year ${new Date().getFullYear() + years}.`
+
+    document.getElementById("result").innerHTML = message;
+
+  
+  }
